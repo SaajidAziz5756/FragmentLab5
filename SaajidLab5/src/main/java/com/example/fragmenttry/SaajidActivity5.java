3f7281ac297a8421bc5a996463d7b1c73fea40c7
SaajidAziz5756 <saajid.aziz1@hotmail.com>
2024-06-12 21:43:41 -0400

    Added back function

--- SaajidLab5/src/main/java/com/example/fragmenttry/SaajidActivity5.java
+++ SaajidLab5/src/main/java/com/example/fragmenttry/SaajidActivity5.java
@@ -1,8 +1,11 @@
 //Saajid Aziz   n01555756
 package com.example.fragmenttry;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class SaajidActivity5 extends AppCompatActivity {
@@ -11,5 +14,29 @@ public class SaajidActivity5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                aDialog();
+            }
+        };
+
+        getOnBackPressedDispatcher().addCallback(this, callback);
+    }
+
+
+    public void aDialog() {
+        new AlertDialog.Builder(SaajidActivity5.this)
+                .setIcon(R.drawable.ic_launcher_background)
+                .setMessage("Would you like to exit the app?")
+                .setTitle(getString(R.string.app_name))
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog,int which ) {finishAffinity();}
+                })
+                .setNegativeButton("No", null)
+                .setCancelable(false)
+                .show();
+
     }
 }
